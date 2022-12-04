150d750e7978ca0ac1e11a9035a27dd9d77cb0da
Joren Dandois <joren.dandois@lunar.be>
2022-12-04 11:35:51 +0100

    December 04 2022

---
+++ 2022/december_04_1.php
@@ -0,0 +1,16 @@
+<?php
+
+$csv = array_map('str_getcsv', file('./files/december_04_1.csv'));
+$overlaps = 0;
+
+foreach ($csv as $key => $line) {
+    $pair = [explode('-', $line[0]), explode('-', $line[1])];
+    if (
+        ($pair[0][0] >= $pair[1][0] && $pair[0][1] <= $pair[1][1]) ||
+        ($pair[0][0] <= $pair[1][0] && $pair[0][1] >= $pair[1][1])
+    ) {
+        $overlaps++;
+    }
+}
+
+print_r($overlaps);
