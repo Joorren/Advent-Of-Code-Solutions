09456fc626802d98b68516add2c12d9d2114d3c0
Joren Dandois <joren.dandois@lunar.be>
2022-12-06 19:45:02 +0100

    December 06 2022

---
+++ 2022/december_06_2.php
@@ -0,0 +1,17 @@
+<?php
+
+$csv = array_map('str_getcsv', file('./files/december_06_1.csv'));
+$letters = [];
+$dataStream = str_split($csv[0][0]);
+
+foreach ($dataStream as $key => $letter) {
+    if (count($letters) === 14) {
+        array_shift($letters);
+    }
+    $letters[] = $letter;
+    if (count($letters) === 14 && array_unique($letters) === $letters) {
+        print_r(implode('', $letters));
+        print_r($key + 1);
+        break;
+    }
+}
